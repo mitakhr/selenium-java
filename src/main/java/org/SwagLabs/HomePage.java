77bf04e9ce577c070f402d748a5d0ece0f313a3a
mitakhr <mitakn08@gmail.com>
2026-09-01 17:10:25 +0700

    add case validate filter list and cart icon

--- src/main/java/org/SwagLabs/HomePage.java
+++ src/main/java/org/SwagLabs/HomePage.java
@@ -2,24 +2,58 @@ package org.SwagLabs;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HomePage {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void showHomePage() {
         driver.get("https://www.saucedemo.com/inventory.html");
     }
 
-    public String  getTitleList(){
+    public String  getHeaderPage(){
         return driver.findElement(By.cssSelector("#header_container > div.header_secondary_container > span")).getText();
     }
 
     public boolean getSortButton(){
-        return driver.findElement(By.className("product_sort_container")).isDisplayed();
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("product_sort_container"))).isDisplayed();
     }
+
+    public List<String> getFilterList(){
+        //Locate the dropdown and initialize the Select class
+        WebElement filterList = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("product_sort_container")));
+        Select dropdownFilter = new Select(filterList);
+
+        //put the value in a list
+        List<String> optionValues = new ArrayList<>();
+
+        for (WebElement option : dropdownFilter.getOptions()) {
+            optionValues.add(option.getText());
+        }
+        return optionValues;
+    }
+
+
+    public boolean getCartButton(){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("shopping_cart_container"))).isDisplayed();
+    }
+
+
 
 }
--- src/test/java/SwagLabs/HomepageTest.java
+++ src/test/java/SwagLabs/HomepageTest.java
@@ -4,10 +4,15 @@ import org.SwagLabs.HomePage;
 import org.SwagLabs.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class HomepageTest {
     WebDriver driver;
@@ -17,10 +22,13 @@ public class HomepageTest {
     @BeforeMethod
     public void beforeMethod() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
         loginPage = new LoginPage(driver);
         loginPage.openLoginPage();
-        driver.manage().window().maximize();
         loginPage.login("standard_user", "secret_sauce");
+
+        homePage = new HomePage(driver);
     }
 
     @AfterMethod
@@ -30,7 +38,6 @@ public class HomepageTest {
 
     @Test
     public void openHomePage() {
-        homePage = new HomePage(driver);
         homePage.showHomePage();
         String currentUrl = driver.getCurrentUrl(); //validate the url
         Assert.assertEquals(currentUrl, "https://www.saucedemo.com/inventory.html");
@@ -38,16 +45,32 @@ public class HomepageTest {
 
     @Test
     public void validateHomePage() {
-        homePage = new HomePage(driver);
-        String titleList = homePage.getTitleList();
+        String titleList = homePage.getHeaderPage();
         Assert.assertEquals(titleList, "Products");
     }
 
     @Test
     public void haveSortButton() {
-        homePage = new HomePage(driver);
         boolean sortButton = homePage.getSortButton();
         Assert.assertTrue(sortButton);
+    }
+
+    @Test
+    public void validateFilterList() {
+        List<String> expectedList = List.of(
+                "Name (A to Z)",
+                "Name (Z to A)",
+                "Price (low to high)",
+                "Price (high to low)");
+
+        List<String> actualValues = homePage.getFilterList();
+        Assert.assertEquals(actualValues, expectedList);
+    }
+
+    @Test
+    public void haveCartButton() {
+        boolean cartButton = homePage.getCartButton();
+        Assert.assertTrue(cartButton);
     }
 
 
